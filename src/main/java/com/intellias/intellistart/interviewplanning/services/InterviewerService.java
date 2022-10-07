b4d945bb872faa0b6863497ead0b253666fc70cc
Oleksii Hytsiv <71726515+holeksii@users.noreply.github.com>
2022-10-07 11:49:38 +0300

    :label:Implement update, get in InterviewerTimeSlot (#30)
    Co-authored-by: Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>

--- src/main/java/com/intellias/intellistart/interviewplanning/services/InterviewerService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/InterviewerService.java
@@ -1,6 +1,10 @@
 package com.intellias.intellistart.interviewplanning.services;
 
 import com.intellias.intellistart.interviewplanning.models.InterviewerTimeSlot;
+import com.intellias.intellistart.interviewplanning.repositories.TimeSlotRepository;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,8 +13,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class InterviewerService {
 
+  private final TimeSlotRepository timeSlotRepository;
+
   /**
-   * crete slot for interview.
+   * Constructor.
+   *
+   * @param timeSlotRepository time slot repository
+   */
+  @Autowired
+  public InterviewerService(TimeSlotRepository timeSlotRepository) {
+    this.timeSlotRepository = timeSlotRepository;
+  }
+
+  /**
+   * Crete slot for interview. Interviewer can create slot for next week.
    *
    * @param from start time
    * @param to   end time
@@ -18,7 +34,38 @@ public class InterviewerService {
    * @return slot
    */
   public InterviewerTimeSlot createSlot(String from, String to, String day, int weekNum) {
-    return new InterviewerTimeSlot(from, to, day, weekNum);
+    // validate from, to, day, weekNum
+    // check if current time is by end of Friday (00:00) of current week
+    return timeSlotRepository.save(new InterviewerTimeSlot(from, to, day, weekNum));
   }
 
+  /**
+   * Get slot by id.
+   *
+   * @param id slot id
+   * @return slotById
+   */
+  public InterviewerTimeSlot getSlot(long id) {
+    return timeSlotRepository.getReferenceById(id);
+  }
+
+  /**
+   * Update slot by id.
+   *
+   * @param id      slot id
+   * @param from    start time
+   * @param to      end time
+   * @param day     day of week
+   * @param weekNum number of week
+   */
+  public InterviewerTimeSlot updateSlot(long id, String from, String to, String day, int weekNum) {
+    // validate from, to, day, weekNum
+    // check if current time is by end of Friday (00:00) of current week
+    InterviewerTimeSlot slot = getSlot(id);
+    slot.setFrom(LocalTime.parse(from));
+    slot.setTo(LocalTime.parse(to));
+    slot.setDayOfWeek(DayOfWeek.valueOf(day));
+    slot.setWeekNum(weekNum);
+    return timeSlotRepository.save(slot);
+  }
 }
